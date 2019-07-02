1ea1b503fae03bfde48ff3fefcf41fe907e98530
DaraZimmermann <dara_zimmermann@estudante.sc.senai.br>
2019-07-02 18:59:11 -0300

    alterações chatbot

--- app.js
+++ app.js
@@ -2,18 +2,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
+
 const app = express();
-const port = 7000;
+const port = 4000;
 
 const AssitantV1 = require('ibm-watson/assistant/v1');
 const assistant = new AssitantV1({ version: '2019-06-01' });
 
+const corsOptions = {
+  origin: '*'
+}
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname ));
+app.use(cors());
 
 app.post('/conversation/', (req, res) => {
     const { text, context = {} } = req.body;
+	res.header("Access-Control-Allow-Origin", "*");
   
     const params = {
         input: { text },
@@ -31,9 +45,10 @@ app.post('/conversation/', (req, res) => {
     });
 });
 
-app.get('/conversation/:text*?', (req, res) => {
+app.get('/conversation/:text*?',(req, res) => {
     console.log(req)
     const { text } = req.params;
+	res.header("Access-Control-Allow-Origin", "*");
 
     res.json(text);
 });
